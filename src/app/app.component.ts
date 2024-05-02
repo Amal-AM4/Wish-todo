d9c85485164de0b92b14f5436b407a7948115f03
Amal A M <vectorhero007@gmail.com>
2024-05-02 18:50:19 +0530

    code restructed compent splited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { WishListComponent } from './wish-list/wish-list.component';
 
+
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    WishListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -23,35 +31,21 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
-  listFilter: string = '0';
+  listFilter: any = '0';
 
   newWishText = '';
 
   title = 'wishlist';
 
-  visibleItems : WishItem[] = this.items;
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
 
   addNewWish() {
     // todo: add wish to items array
     // clear the textbox
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
-  }
-
-  filterChanged(value: any) {
-    if (value === '0') {
-      this.visibleItems = this.items
-    } else if (value === '1') {
-      this.visibleItems = this.items.filter(item => !item.isComplete);
-    } else {
-      this.visibleItems = this.items.filter(item => item.isComplete);
-    }
-
-  }
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
-    console.log(item);
   }
 
 }
