1fd3e7154a318fd76790fa3e7c1e5b97c863cf23
Amal A M <vectorhero007@gmail.com>
2024-05-03 09:56:19 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,8 @@ import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
 
-
-const filters = [
-  (item: WishItem) => item,
-  (item: WishItem) => !item.isComplete,
-  (item: WishItem) => item.isComplete,
-];
 
 @Component({
   selector: 'app-root',
@@ -20,12 +15,12 @@ const filters = [
     CommonModule,
     FormsModule,
     WishListComponent,
-    AddWishFormComponent
+    AddWishFormComponent,
+    WishFilterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
   items: WishItem[] = [
     new WishItem('Learn Angular'),
@@ -33,13 +28,6 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
-  listFilter: any = '0';
-
-  title = 'wishlist';
-
-  get visibleItems() : WishItem[] {
-    return this.items.filter(filters[this.listFilter]);
-  }
-
+  filter: any = () => true;
 
 }
